777617d1994071a555a10de1eeb3afd5d5eaecbc
emran <shuvoorba28@gmail.com>
2023-09-25 16:50:08 +0600

    button photo title and description added in the card detail page

--- src/pages/CartDetails/CartDetails.jsx
+++ src/pages/CartDetails/CartDetails.jsx
@@ -10,10 +10,8 @@ const CartDetails = () => {
 
     const { id } = useParams();
     const idInt = parseInt(id);
-    // console.log(+id)
 
     const cardDetail = useLoaderData();
-    console.log(cardDetail);
 
     useEffect(() => {
 
@@ -26,14 +24,14 @@ const CartDetails = () => {
 
     return (
         <div>
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img src={detail.picture} alt="Shoes" /></figure>
+            <div className="card w-auto bg-base-100 shadow-xl">
+                <figure><img className="w-full relative h-[700px]" src={detail.picture} alt="Shoes" /></figure>
+                <div className="bg-black h-32  flex relative -mt-32 bg-opacity-50 justify-start">
+                    <button className="rounded-lg hover:shadow-xl shadow-gray-400 w-44 h-14 border-none mt-9 ms-9 text-white bg-[#FF444A]">Donate {detail.price}</button>
+                </div>
                 <div className="card-body">
                     <h2 className="card-title">Shoes!</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
                 </div>
             </div>        </div>
     );
