8b305df7ff6156de63d29f1e1e1b72c0cf261da6
emran <shuvoorba28@gmail.com>
2023-09-26 12:03:56 +0600

    card details color added for button, category, category background, card background

--- src/pages/donation/DonatedItem.jsx
+++ src/pages/donation/DonatedItem.jsx
@@ -1,18 +1,18 @@
 import PropTypes from 'prop-types'
 
 const DonatedItem = ({ donated }) => {
-    const { picture, price, title, category } = donated || {};
+    const { picture, price, title, category, card_bg_color, text_color, category_bg_color } = donated || {};
     console.log(donated);
     return (
         <div>
-            <div className="card mt-8 card-side bg-base-100 shadow-xl">
+            <div style={{ backgroundColor: card_bg_color }} className="card ms-6 p-4 mt-8 card-side bg-base-100 shadow-xl">
                 <figure><img src={picture} alt="Movie" /></figure>
                 <div className="card-body">
-                    <p>{category}</p>
+                    <p className='w-1/4 rounded-md p-1' style={{ color: text_color, backgroundColor: category_bg_color }} >{category}</p>
                     <h4 className="card-title">{title}</h4>
                     <p>{price}</p>
                     <div className="card-actions justify-start">
-                        <button className="btn btn-primary">View details </button>
+                        <button style={{ backgroundColor: text_color }} className="btn text-white">View details </button>
                     </div>
                 </div>
             </div>
