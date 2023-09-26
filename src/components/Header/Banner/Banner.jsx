7647a676a3b31d5ada077f1a2ffae645becbaf92
emran <shuvoorba28@gmail.com>
2023-09-26 08:15:25 +0600

    banner background image added

--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -2,10 +2,12 @@ import './banner.css'
 
 const Banner = () => {
     return (
-        <div className=" banner-img text-center mb-52 mt-36">
-             <h2 className="text-5xl font-bold mb-6">I Grow By Helping People In Need</h2> 
-             <input className="h-9 ps-3 mt-5" placeholder="search here" type="text" />          
-             <button className="p-2 rounded-e-xl hover:bg-red-700 hover:shadow-xl hover:shadow-red-200 hover:text-gray-200 text-white bg-[#ff444a]">Search</button>
+        <div className="banner-img flex flex-col justify-center text-center h-[50vh] mb-20">
+            <h2 className="text-5xl font-bold mb-6 text-rose-600">I Grow By Helping People In Need</h2>
+            <div>
+                <input className="h-10  ps-3 mt-5" placeholder="search here" type="text" />
+                <button className="p-2 rounded-e-xl hover:bg-red-700 hover:shadow-xl hover:shadow-red-200 hover:text-gray-200 text-white bg-[#ff444a]">Search</button>
+            </div>
         </div>
     );
 };
