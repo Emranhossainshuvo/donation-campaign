6989f3e6a02efd9f24d1c02a9a423f0e7642edd5
emran <shuvoorba28@gmail.com>
2023-09-26 07:56:49 +0600

    show more functionality added

--- src/pages/donation/Donation.jsx
+++ src/pages/donation/Donation.jsx
@@ -10,32 +10,40 @@ const Donation = () => {
 
     const [isShow, setIsShow] = useState(false)
 
-    useEffect(()=> {
+    useEffect(() => {
         const donationItems = JSON.parse(localStorage.getItem('donation-data'))
-        if(donationItems){
+        if (donationItems) {
             setDonation(donationItems)
-        }else{
+        } else {
             setNoFound("No data found")
         }
     }, [])
-    
+
     console.log(isShow);
     console.log(donation)
 
     return (
         <div>
-            {noFound ? <p className="flex justify-center items-center text-4xl font-bold h-[70vh]"> {noFound} </p> : 
-            // donated cards here
-            <div>
-                <div className="grid grid-cols-1 md:grid-cols-2">
-                    {
-                        donation?.map(item =><DonatedItem key={item.id} donated={item}>
+            {noFound ? <p className="flex justify-center items-center text-4xl font-bold h-[70vh]"> {noFound} </p> :
+                // donated cards here
+                <div>
+                    <div className="grid grid-cols-1 md:grid-cols-2">
+                        {
+                            isShow 
+                            ?
+                             donation?.map(item => <DonatedItem key={item.id} donated={item}>
 
-                        </DonatedItem>)
-                    }
-                </div>
-                <button onClick={()=> setIsShow(!isShow)} className="bg-green-700 hover:bg-green-800 hover:shadow-green-950 hover:shadow-sm text-white p-2 rounded-md">See more</button>
-            </div> }
+                            </DonatedItem>) 
+                            :
+                             donation?.slice(0, 4).map(item => <DonatedItem key={item.id} donated={item}>
+
+                            </DonatedItem>)
+                        }
+                    </div>
+                   {
+                    donation.length > 4 &&  <button onClick={() => setIsShow(!isShow)} className="bg-green-700 block mx-auto px-5 mt-6 hover:bg-green-800 hover:shadow-green-950 hover:shadow-sm text-white p-2 rounded-md">{isShow ? "Show less" : "See more"}</button>
+                   }
+                </div>}
         </div>
     );
 };
