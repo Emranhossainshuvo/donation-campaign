02f3131e3b4eb2580c599e9a5d276413b4c98137
emran <shuvoorba28@gmail.com>
2023-09-27 13:20:15 +0600

    pie chart added

--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -1,19 +1,48 @@
 import { useLoaderData } from 'react-router-dom';
+import React from 'react';
+import Chart from 'react-apexcharts'
+
 // import { TotalDataContext } from '../../components/Carts/Carts';
 
 
 
 
 const Statistics = () => {
-    const data = useLoaderData(); 
+    const totalData = useLoaderData(); 
 
     const localData = localStorage.getItem('donation-data'); 
-    console.log(JSON.parse(localData))
+    const localStoredData = JSON.parse(localData);
+
+    const donatedData = localStoredData.length; 
+    const data = totalData.length; 
+
+    const donatedParcentage = (donatedData / data) * 100; 
+
+    const homeParcentage = 100 - donatedParcentage
 
     console.log(data)
     return (
         <div>
+            <React.Fragment>
+                <div className='flex justify-center items-center mt-20'>
+                    <Chart
+                    type='pie'
+                    width={1300}
+                    height={550}
 
+                    series={[donatedParcentage, homeParcentage]}
+
+                    options={{
+                        colors: ['#FF444A', '#00C49F'],
+
+                        labels: ["Your donation", "Total donation"]
+                    }}  
+
+                    >
+
+                    </Chart>
+                </div>
+            </React.Fragment>
         </div>
     
     );
